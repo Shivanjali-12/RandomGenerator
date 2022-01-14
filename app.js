0834316459cd576ed4097e057af5d37d9e4a88dd
Shivanjali-12 <shivanjali.rajput13@gmail.com>
2022-01-14 19:18:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const generateBtn = document.querySelector(".generateBtn");
+
+generateBtn.addEventListener("click", generateRandomNum);
+
+function generateRandomNum() {
+  const fromNum = document.querySelector(".fromNum").value;
+  const toNum = document.querySelector(".toNum").value;
+
+  randomNum = Math.floor(Math.random() * parseInt(toNum-fromNum)) + parseInt(fromNum);
+
+  const randomNumEl = document.querySelector(".randomNumEl");
+  randomNumEl.innerHTML = randomNum;
+}
